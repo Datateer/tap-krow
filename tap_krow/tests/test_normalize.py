397c26a31c89652de7824bf6ea751a407cb68b32
Adam Roderick <aroder@gmail.com>
2022-01-10 14:41:17 -0700

    feat: fix data types and change dot notation to underscore so that it works with meltano's select syntax

--- tap_krow/tests/test_normalize.py
+++ tap_krow/tests/test_normalize.py
@@ -1,13 +1,13 @@
 import pytest
-from tap_krow.normalize import flatten_dict, remove_unnecessary_keys
+from tap_krow.normalize import flatten_dict, remove_unnecessary_keys, make_fields_meltano_select_compatible
 
 
 def test_flattens_dict():
     d = {"a": {"a": "hi"}}
     flattened = flatten_dict(d)
-    assert "a.a" in flattened
+    assert "a_a" in flattened
     assert "a" not in flattened
-    assert flattened["a.a"] == d["a"]["a"]
+    assert flattened["a_a"] == d["a"]["a"]
 
 
 def test_accepts_delimiter():
@@ -22,9 +22,9 @@ def test_removes_extraneous_data_key():
     d = {"a": {"data": {"b": {"data": {"c": "hi"}}}}}
     purged = remove_unnecessary_keys(d, keys_to_remove=["data"])
     flattened = flatten_dict(purged)
-    assert "a.b.c" in flattened
-    assert "a.data.b.data.c" not in flattened
-    assert flattened["a.b.c"] == d["a"]["data"]["b"]["data"]["c"]
+    assert "a_b_c" in flattened
+    assert "a_data_b_data_c" not in flattened
+    assert flattened["a_b_c"] == d["a"]["data"]["b"]["data"]["c"]
 
 
 def test_errors_if_child_key_already_exists_in_parent():
@@ -36,3 +36,14 @@ def test_errors_if_child_key_already_exists_in_parent():
         str(exc_info.value)
         == 'The key "a" already exists in the output, so cannot promote the child of key "to_be_removed"'
     )
+
+
+def test_make_fields_meltano_select_compatible_replaces_dots_with_underscores_in_field_name():
+    d = {"opening.position": "hi", "nested": {"a.b": "hi2"}}
+    cleaned = make_fields_meltano_select_compatible(d)
+    assert "opening.position" not in cleaned
+    assert "opening_position" in cleaned
+    assert cleaned["opening_position"] == "hi"
+    assert "a.b" not in cleaned["nested"]
+    assert "a_b" in cleaned["nested"]
+    assert cleaned["nested"]["a_b"] == "hi2"
