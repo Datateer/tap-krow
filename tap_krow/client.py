0912ed684b71a8c5ab686f06b59183493cbba671
Chris Marchetti <cmarche2ti@gmail.com>
2022-09-06 17:17:00 -0600

    updated backoff logic

--- tap_krow/client.py
+++ tap_krow/client.py
@@ -13,7 +13,11 @@ from singer_sdk.exceptions import RetriableAPIError
 from singer_sdk.helpers.jsonpath import extract_jsonpath
 from singer_sdk.streams import RESTStream
 from tap_krow.auth import krowAuthenticator
-from tap_krow.normalize import flatten_dict, remove_unnecessary_keys, make_fields_meltano_select_compatible
+from tap_krow.normalize import (
+    flatten_dict,
+    remove_unnecessary_keys,
+    make_fields_meltano_select_compatible,
+)
 
 
 class KrowStream(RESTStream):
@@ -56,6 +60,19 @@ class KrowStream(RESTStream):
         )(func)
         return decorator
 
+    def backoff_wait_generator(self):  # -> Callable[..., Generator[int, Any, None]]:
+        """The wait generator used by the backoff decorator on request failure.
+
+        See for options:
+        https://github.com/litl/backoff/blob/master/backoff/_wait_gen.py
+
+        And see for examples: `Code Samples <../code_samples.html#custom-backoff>`_
+
+        Returns:
+            The wait generator
+        """
+        return backoff.expo(base=4, factor=2)  # type: ignore # ignore 'Returning Any'
+
     def get_next_page_url(self, response: requests.Response):
         matches = extract_jsonpath(self.next_page_url_jsonpath, response.json())
         next_page_url = next(iter(matches), None)
@@ -63,13 +80,17 @@ class KrowStream(RESTStream):
 
     def get_earliest_timestamp_in_response(self, response: requests.Response):
         """This assumes the response is sorted in descending order"""
-        matches = extract_jsonpath(f"$.data[-1:].attributes.{self.replication_key}", response.json())
+        matches = extract_jsonpath(
+            f"$.data[-1:].attributes.{self.replication_key}", response.json()
+        )
         earliest_timestamp_in_response = next(iter(matches), None)
         if earliest_timestamp_in_response is None:
             return None
         return dateutil.parser.parse(earliest_timestamp_in_response)
 
-    def get_next_page_token(self, response: requests.Response, previous_token: Optional[Any]) -> Optional[Any]:
+    def get_next_page_token(
+        self, response: requests.Response, previous_token: Optional[Any]
+    ) -> Optional[Any]:
         """Return a token for identifying next page or None if no more pages.
         For KROW, the only option is to sort in descending order, so we return only if earliest time in response < stop point
         We use a dictionary here to keep track of the stop point and the current page,
@@ -93,7 +114,10 @@ class KrowStream(RESTStream):
         state = self.get_context_state(None)
         # if stop_point is None, then no state was passed in, and we want all records
         # if stop_point is < the earliest timestamp in the response, we want to get the next page
-        if previous_token["stop_point"] is None or previous_token["stop_point"] < earliest_timestamp:
+        if (
+            previous_token["stop_point"] is None
+            or previous_token["stop_point"] < earliest_timestamp
+        ):
             next_page_url = self.get_next_page_url(response)
             if next_page_url is None:
                 logging.info("There are no more pages; reached the end of the records")
@@ -106,7 +130,10 @@ class KrowStream(RESTStream):
             if next_page_url:
                 search = re.search("page%5Bnumber%5D=(\\d+)", next_page_url)
                 if search:
-                    next_page_token = {**previous_token, "current_page": int(search.group(1))}
+                    next_page_token = {
+                        **previous_token,
+                        "current_page": int(search.group(1)),
+                    }
         else:
             logging.info(
                 f"""{previous_token["stop_point"]} is later than {earliest_timestamp}
@@ -117,7 +144,9 @@ class KrowStream(RESTStream):
 
         return next_page_token
 
-    def get_url_params(self, context: Optional[dict], next_page_token: Optional[Any]) -> Dict[str, Any]:
+    def get_url_params(
+        self, context: Optional[dict], next_page_token: Optional[Any]
+    ) -> Dict[str, Any]:
         """Return a dictionary of values to be used in URL parameterization."""
         params: dict = {
             "page[size]": self.page_size,
@@ -163,11 +192,16 @@ class KrowStream(RESTStream):
             d = flatten_dict(d)
 
             # remove extraneous keys that are not in the stream's schema
-            keys_to_remove = [k for k in d.keys() if k not in properties_defined_in_schema]
+            keys_to_remove = [
+                k for k in d.keys() if k not in properties_defined_in_schema
+            ]
             d = remove_unnecessary_keys(d, keys_to_remove)
 
             # short circuit if we encounter records from earlier than our stop_point
-            if d["updated_at"] is None or (stop_point is not None and dateutil.parser.parse(d["updated_at"]) < stop_point):
+            if d["updated_at"] is None or (
+                stop_point is not None
+                and dateutil.parser.parse(d["updated_at"]) < stop_point
+            ):
                 logging.info(
                     f"""This record\'s updated_at = {d["updated_at"]} which is less than the stop point{stop_point}.
                     Will not return any more records, because they were synced earlier"""
